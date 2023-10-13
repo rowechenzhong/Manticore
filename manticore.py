f002c1ca502371519e05cbba0c91aafbb1cb1337
Rowechen Zhong <rowechen@gmail.com>
2023-10-13 01:11:05 -0400

    Huh, does it work

--- manticore.py
+++ manticore.py
@@ -14,7 +14,7 @@ class Manticore:
     This class comprises a model and tokenizer, 
     """
 
-    def __init__(self, model: Model, tokenizer: Tokenizer, device: str = "cuda") -> None:
+    def __init__(self, model: Model, tokenizer: Tokenizer, device: str = "cpu") -> None:
         self.model = model.to(device)
         self.tokenizer = tokenizer
         self.device = device
@@ -22,12 +22,18 @@ class Manticore:
     def train(self, train_corpus: str,
               test_corpus: str,
               size: int = 100,
-              batch_size: int = 1000,
+              batch_size: int = 100,
               epochs: int = 10) -> None:
         """
         Train the model on a corpus, which is just a list.
         The model will be trained to predict the next token,
         and the loss will be cross-entropy.
+
+        :param train_corpus: The corpus to train on.
+        :param test_corpus: The corpus to test on.
+        :param size: The length of a training example. (Called seq_len elsewhere)
+        :param batch_size: The batch size.
+        :param epochs: The number of epochs to train for.
         """
         # tokenized_train_corpus = torch.tensor(
         #     self.tokenizer.tokenize(train_corpus))
@@ -36,12 +42,19 @@ class Manticore:
             self.tokenizer.tokenize(train_corpus), dtype=torch.int64)
 
         train = torch.utils.data.DataLoader(
-            SubstringDataset(tokenized_train_corpus, size), batch_size=batch_size, shuffle=True).to(self.device)
+            SubstringDataset(tokenized_train_corpus, size), batch_size=batch_size, shuffle=True, pin_memory=True)
 
         tokenized_test_corpus: torch.Tensor = torch.tensor(
             self.tokenizer.tokenize(test_corpus), dtype=torch.int64)
         test = torch.utils.data.DataLoader(
-            SubstringDataset(tokenized_test_corpus, size), batch_size=batch_size, shuffle=True).to(self.device)
+            SubstringDataset(tokenized_test_corpus, size), batch_size=batch_size, shuffle=True, pin_memory=True)
+        
+        # The purpose of pin_memory is to speed up the transfer of data from CPU to GPU, which is done by transferring
+        # the data to the pinned memory first and then transferring it to the GPU. This is because the data is transferred
+        # from the CPU to the GPU through the PCI-E bus. The PCI-E bus is shared by all devices on the motherboard, 
+        # and the data transfer is slow. The pinned memory is allocated in the memory area that is not shared by the
+        # PCI-E bus, so the data transfer is fast. The disadvantage is that the pinned memory cannot be
+        # shared by multiple processes, so it is not suitable for multi-process data reading.
 
         print("Size of training set:", len(train))
         print("Size of test set:", len(test))
@@ -53,7 +66,9 @@ class Manticore:
             print(f"Epoch {epoch}")
             trainings_loss = 0
             self.model.train()
-            for x, y in train:
+            for x, y in tqdm(train):
+                x = x.to(self.device)
+                y = y.to(self.device)
                 optimizer.zero_grad()
                 # print("In train loop")
                 # print(x.shape, y.shape)
@@ -67,7 +82,9 @@ class Manticore:
             test_loss = 0
             self.model.eval()
             with torch.no_grad():
-                for x, y in test:
+                for x, y in tqdm(test):
+                    x = x.to(self.device)
+                    y = y.to(self.device)
                     y_pred = self.model(x).permute(0, 2, 1)
                     l = loss(y_pred, y)
                     test_loss += l.item()
@@ -77,57 +94,88 @@ class Manticore:
         """
         Generate a string of a given length from a given seed.
         """
-        tokenized_seed: List[int] = torch.tensor(
-            self.tokenizer.tokenize(seed))
+        tokenized_seed: torch.Tensor = torch.tensor(
+            self.tokenizer.tokenize(seed)).unsqueeze(0) # (1, seq_len)
+        
         self.model.eval()
         with torch.no_grad():
             for _ in range(length):
                 y_pred = self.model(tokenized_seed[-100:])
-                tokenized_seed = torch.cat(
-                    (tokenized_seed, torch.argmax(y_pred, dim=1)[-1].unsqueeze(0)))
-        return self.tokenizer.detokenize(tokenized_seed.tolist())
+                print(y_pred.shape)
+                # only take the last token
+                y_pred = y_pred[:, -1, :].unsqueeze(1)
+                result = torch.argmax(y_pred, dim=2)
+                print(tokenized_seed.shape, result.shape)
+
+                tokenized_seed = torch.cat((tokenized_seed, result), dim=1)
+        tokenized_seed = tokenized_seed.squeeze(0).tolist()
+        return self.tokenizer.detokenize(tokenized_seed)
 
     def save(self, path: str) -> None:
         """
         Save the model and tokenizer to a given path.
         """
         torch.save(self.model.state_dict(), path + ".model")
-        self.tokenizer.save(path + ".tokenizer")
 
     def load(self, path: str) -> None:
         """
         Load the model and tokenizer from a given path.
         """
         self.model.load_state_dict(torch.load(path + ".model"))
-        self.tokenizer.load(path + ".tokenizer")
+
+########## Nerfed test ##########
 
 
 if __name__ == "__main__":
+    # detect device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+
+    NERFED = True
+
+    if NERFED:        
+        corpus = open("corpus\\communistmanifesto.txt", "rb").read()
+        corpus = "".join([chr(i) for i in corpus])
+        train_corpus = corpus[:int(0.8 * len(corpus))]
+        test_corpus = corpus[int(0.8 * len(corpus)):]
+
+        SIZE = 20
+        LAYERS = 2
+        BATCH_SIZE = 20
+        EPOCHS = 2
+    else:
+        corpus1 = open("corpus\\mahabharata1.txt", "rb").read()
+        corpus1 = "".join([chr(i) for i in corpus1])
+        corpus2 = open("corpus\\mahabharata2.txt", "rb").read()
+        corpus2 = "".join([chr(i) for i in corpus2])
+        corpus3 = open("corpus\\mahabharata3.txt", "rb").read()
+        corpus3 = "".join([chr(i) for i in corpus3])
+
+        train_corpus = corpus1 + corpus2
+        test_corpus = corpus3
+
+        SIZE = 64
+        LAYERS = 8
+        BATCH_SIZE = 100
+        EPOCHS = 10
+
     tokenizer = Tokenizer()
-    tokenizer.load(
-        "./tokenizers/tokenizer_outputs/mahabharata_size4000_cap10.txt")
+    tokenizer.load("./tokenizers/tokenizer_outputs/mahabharata_size4000_cap10.txt")
 
-    corpus1 = open("corpus\\mahabharata1.txt", "rb").read()
-    corpus1 = "".join([chr(i) for i in corpus1])
-    corpus2 = open("corpus\\mahabharata2.txt", "rb").read()
-    corpus2 = "".join([chr(i) for i in corpus2])
-    corpus3 = open("corpus\\mahabharata3.txt", "rb").read()
-    corpus3 = "".join([chr(i) for i in corpus3])
-    corpus = corpus1 + corpus2 + corpus3
+    embedding_in = Embedding(len(tokenizer), SIZE)
+    embedding_out = UnEmbedding(len(tokenizer), SIZE)
 
-    train_corpus = corpus1 + corpus2
-    test_corpus = corpus3
+    model = Model(embedding_in, embedding_out, size=SIZE, layers=LAYERS)
+    manticore = Manticore(model, tokenizer, device=device)
 
-    embedding_in = Embedding(len(tokenizer), 64)
-    embedding_out = UnEmbedding(len(tokenizer), 64)
-
-    model = Model(embedding_in, embedding_out, size=64, layers=8)
-    manticore = Manticore(model, tokenizer)
-
-    manticore.train(train_corpus, test_corpus)
-    # prayge
+    # manticore.train(train_corpus, test_corpus,
+    #                 size=SIZE, batch_size=BATCH_SIZE, epochs=EPOCHS)
+    
+    # manticore.save("manticore")
     print(manticore.generate("The ", 100))
-
-    manticore.save("manticore")
+    
     manticore.load("manticore")
+    # prayge
+
     print(manticore.generate("The ", 100))
+
